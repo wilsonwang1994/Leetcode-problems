504a4919f42ba0da2a999588841461d72ffa7f63
Wilson Wang <49656014+wilsonwang1994@users.noreply.github.com>
2019-05-22 08:41:55 -0700

    Add files via upload

--- 1-100/2-Add Two Numbers/Solution.java
+++ 1-100/2-Add Two Numbers/Solution.java
@@ -8,6 +8,19 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
+        int carry = 0;
+        int sum = 0;
+        ListNode result = new ListNode(0);
+        ListNode curNode = result;
+        while(l1 != null || l2 != null || carry != 0){
+            curNode.next = new ListNode(0);
+            curNode = curNode.next;
+            sum = ((l1 == null) ? 0:l1.val) + ((l2 == null) ? 0:l2.val) + carry;
+            curNode.val = sum % 10;
+            carry = sum / 10;
+            l1 = ((l1 == null) ? l1:l1.next);
+            l2 = ((l2 == null) ? l2:l2.next);
+        }
+        return result.next;
     }
 }
